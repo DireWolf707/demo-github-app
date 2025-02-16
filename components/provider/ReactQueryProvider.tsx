f2376c3c80cf154c2b379a58e7b7023e1664236e
Direwolf <rahulsood707@gmail.com>
2025-02-16 11:47:10 +0530

    added shadcn and tanstack

---
+++ components/provider/ReactQueryProvider.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { PropsWithChildren, useState } from "react"
+import React from "react"
+
+const ReactQueryProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+            retry: false,
+            retryOnMount: false,
+          },
+        },
+      })
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
+
+export default ReactQueryProvider
---
+++ components/provider/ThemeProvider.tsx
@@ -0,0 +1,16 @@
+"use client"
+import * as React from "react"
+import { Toaster } from "@/components/ui/sonner"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export default function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+      <Toaster position="top-right" />
+    </NextThemesProvider>
+  )
+}
